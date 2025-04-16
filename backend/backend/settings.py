214a96d555cfaa6f3441abb0ba8009ddd5bd8288
Maks Marcenukm <marcenukm995@gmail.com>
2025-04-16 23:35:07 +0300

    was created app for notes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'Users_Api'
+    'notes'
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Users_Api.urls')),
+    path('notes-api/', include('notes.urls')),
     
 ]
