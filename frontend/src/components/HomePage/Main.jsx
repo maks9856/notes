8e6f28624a73b2065fbf559a276988f4637a081e
Maks Marcenukm <marcenukm995@gmail.com>
2025-05-01 00:32:38 +0300

    was made header of main part of page

---
+++ frontend/src/components/HomePage/Main.jsx
@@ -0,0 +1,31 @@
+import '../../styles/HomePage/Main.css';
+import hero_img from '../../assets/hero.jpg';
+import { useNavigate } from 'react-router-dom';
+export default function Main() {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/register');
+    }
+    return (
+        <main>
+            <div className='main__homepage'>
+            <section className="main__hero">
+                <div className="hero__content">
+                    <h1 className="hero__title">The happier workspace</h1>
+                    <h2 className="hero__subtitle">Write. Plan. Collaborate. With a little help from AI.</h2>
+                    <div className="hero__buttons">
+                        <button className="hero__button" onClick={handleClick}>Sign up</button>
+                    </div>
+                    <div className="hero__logoGrouop">
+                       <span className="hero__trusted-by">Trusted by teams at</span>
+                    </div>
+                </div>
+                <div className='img__content'>
+                    <img className='img__hero' src={hero_img} alt="" />
+                </div>
+            </section>
+            </div>
+           
+        </main>
+    );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/HomePage/Header";
+import Main from "../components/HomePage/Main";
 
 function Home(){
     return (
         <>
             <Header></Header>
+            <Main></Main>
         </>
     )
 }
